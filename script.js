7cc494ed65d0e2fefc79e73d16d90ec1c0b2b8dd
HenrikH <henrik.hellem@live.com>
2023-09-06 14:54:46 +0200

    feat: background change color

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let defaultGridSize = 16;
 
-function createGridChildren(gridSize){
-    const gridParent = document.querySelector('.grid-parent')
-    for (let i = 0; i < gridSize; i++){
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-child');
-        gridSquare.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'black';
-        })
-        gridParent.appendChild(gridSquare);
-    }
-
+function createGridChildren(gridSize) {
+  const gridParent = document.querySelector(".grid-parent");
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-child");
+    gridSquare.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = "black";
+    });
+    gridParent.appendChild(gridSquare);
+  }
 }
 
-createGridChildren(defaultGridSize)
+createGridChildren(defaultGridSize);
