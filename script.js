aed5ecd4940673f3f9a32695d13a9a03a5c4870b
HenrikH <henrik.hellem@live.com>
2023-09-06 17:24:07 +0200

    Update main func to be able to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let defaultGridSize = 16;
+let defaultGridSize = 121;
+
 
 function createGridChildren(gridSize) {
-  const gridParent = document.querySelector(".grid-parent");
-  for (let i = 0; i < gridSize ** 2; i++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid-child");
-    gridSquare.addEventListener("mouseover", function (event) {
+    const gridParent = document.querySelector(".grid-parent");
+    const cellSize = 780 / gridSize;
+    
+    const handleMouseOver = (event) => {
       event.target.style.backgroundColor = "black";
-    });
-    gridParent.appendChild(gridSquare);
+    };
+    
+    for (let i = 0; i < gridSize ** 2; i++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.style.cssText = `width: ${cellSize}px; height: ${cellSize}px;`;
+      gridSquare.classList.add("grid-child");
+      gridSquare.addEventListener("mouseover", handleMouseOver);
+      gridParent.appendChild(gridSquare);
+    };
   }
-}
 
 createGridChildren(defaultGridSize);
