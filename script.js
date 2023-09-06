2924a1307ebdf09150dd95e4d009c1e760cf0ca2
HenrikH <henrik.hellem@live.com>
2023-09-06 10:32:21 +0200

    Make grid function actually work

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const gridParent = document.querySelector('grid-parent')
+let defaultGridSize = 16;
 
-function createGridChildren(){
-    let gridSize = 16;
+function createGridChildren(gridSize){
+    const gridParent = document.querySelector('.grid-parent')
     for (let i = 0; i < gridSize; i++){
         const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-child')
-        gridParent.appendChild(gridSquare)
+        gridSquare.classList.add('grid-child');
+        gridSquare.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        gridParent.appendChild(gridSquare);
     }
 
 }
+
+createGridChildren(defaultGridSize)
