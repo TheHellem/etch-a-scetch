32db947b45736fc596f191e61600eec65df96196
HenrikH <henrik.hellem@live.com>
2023-10-12 20:05:31 +0200

    Add Eventlistener slider

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+let gridSize = 64;
 
-function detectMode () {
-
+function setMode(mode = "default") {
+  // her er ideeen å ha if for hvile mode det er
+  return [mode];
 }
 
-function createGridChildren(gridSize = 64) {
+function createGridChildren() {
+  const cellSize = 780 / gridSize;
+  
+  const handleMouseOver = (event) => {
+    event.target.style.backgroundColor = "black";
+  };
+  
+  for (let i = 0; i < gridSize ** 2; i++) {
     const gridParent = document.querySelector(".grid-parent");
-    const cellSize = 780 / gridSize;
-    
-    const handleMouseOver = (event) => {
-      event.target.style.backgroundColor = "black";
-    };
-    
-    for (let i = 0; i < gridSize ** 2; i++) {
-      const gridSquare = document.createElement("div");
-      gridSquare.style.cssText = `width: ${cellSize}px; height: ${cellSize}px;`;
-      gridSquare.classList.add("grid-child");
-      gridSquare.addEventListener("mouseover", handleMouseOver);
-      gridParent.appendChild(gridSquare);
-    };
+    const gridSquare = document.createElement("div");
+    gridSquare.style.cssText = `width: ${cellSize}px; height: ${cellSize}px;`;
+    gridSquare.classList.add("grid-child");
+    gridSquare.addEventListener("mouseover", handleMouseOver);
+    gridParent.appendChild(gridSquare);
   }
+}
 
-createGridChildren(99)
+const slider = document.querySelector("#slider");
+
+slider.addEventListener("input", (e) => {
+  gridSize = document.querySelector("#slider").value;
+  console.log(gridSize);
+});
+
+createGridChildren();
